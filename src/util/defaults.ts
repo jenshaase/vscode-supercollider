8cfb612241abc8051e55ad6351afe4da721bcc5d
scott carver <scott@artificia.org>
2024-04-19 10:43:15 +0200

    Improve searches for windows install paths

--- src/util/defaults.ts
+++ src/util/defaults.ts
@@ -41,11 +41,42 @@ export function sclangConfYamlPath() {
     return path.join(userConfigPath(), sclangConfYaml)
 }
 
+function getInstallPathWin() {
+    const MAX_MAJOR_VERSION = 20
+    const MAX_MINOR_VERSION = 6
+
+    const minorVersions = new Array(MAX_MAJOR_VERSION + 1)
+        .fill(null)
+        .map((_, i) => i)
+        .reverse();
+
+    const patchVersions = new Array(MAX_MINOR_VERSION + 1)
+        .fill(null)
+        .map((_, i) => i)
+        .reverse();
+
+    try {
+        return GetStringRegKey('HKEY_CURRENT_USER', `SOFTWARE\\${ApplicationName}\\CurrentVersion`, '')
+    } catch { }
+
+    for (const minor of minorVersions) {
+        for (const patch of patchVersions) {
+            const versionString = `3.${minor}.${patch}`;
+            try {
+                return GetStringRegKey('HKEY_CURRENT_USER', `SOFTWARE\\${ApplicationName}\\${versionString}`, '')
+            } catch { }
+        }
+    }
+
+    return 'c:\\Program Files\\SuperCollider';
+}
+
+getInstallPathWin()
+
 export function sclangPath() {
     switch (platform()) {
         case 'win32': {
-            const installPath = GetStringRegKey('HKEY_LOCAL_MACHINE', `SOFTWARE\\${ApplicationName}\\CurrentVersion`, '') || 'c:\\Program Files\\SuperCollider';
-            return path.join(installPath, sclangExecutable());
+            return path.join(getInstallPathWin(), sclangExecutable());
         }
         case 'darwin': {
             return path.join('/Applications', ApplicationName + '.app', 'Contents', 'MacOS', sclangExecutable())
